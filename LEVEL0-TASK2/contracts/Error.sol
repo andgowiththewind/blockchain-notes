b182337b9b073cd37d251b08e3addb3fcb76ccbd
gust <2414566866@qq.com>
2024-08-10 22:56:45 +0800

    Error

--- LEVEL0-TASK2/contracts/Error.sol
+++ LEVEL0-TASK2/contracts/Error.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+
+/*一个错误会撤销事务期间对状态所做的所有更改。
+
+你可以通过调用 `require`、`revert` 或 `assert` 来抛出错误。
+
+- `require` 用于在执行前验证输入和条件。
+- `revert` 类似于 `require`，用于在条件不满足时撤销操作。详情见下面的代码。
+- `assert` 用于检查应永远为真的代码。断言失败通常意味着存在 bug。
+
+使用自定义错误可以节省 gas。*/
 contract Error {
     function testRequire(uint256 _i) public pure {
         // Require should be used to validate conditions such as:
