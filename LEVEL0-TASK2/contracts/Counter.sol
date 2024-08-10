7aa3211e141343b116de4aeee296f64bd1126861
gust <2414566866@qq.com>
2024-08-10 21:38:14 +0800

    hello world

---
+++ LEVEL0-TASK2/contracts/Counter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+//这是一个简单的合同，你可以获取、增加和减少存储在合同中的计数。
+contract Counter {
+    uint256 public count;
+
+    //获取当前计数的函数
+    function get() public view returns (uint256) {
+        return count;
+    }
+
+    // 将计数增加1的函数
+    function inc() public {
+        count += 1;
+    }
+
+    // 减1
+    function dec() pub {
+        count -= 1;
+    }
+}
---
+++ LEVEL0-TASK2/contracts/Primitives.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// 在这里，我们介绍一些 Solidity 中可用的基本数据类型。
+contract Primitives {
+    constructor(){
+
+    }
+}
