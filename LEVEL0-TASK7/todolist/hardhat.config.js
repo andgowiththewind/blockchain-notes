5a937386d07ac7129f18df1f87a6e692a48aa94c
gust <2414566866@qq.com>
2024-08-16 01:25:33 +0800

    todolist test success

--- LEVEL0-TASK7/todolist/hardhat.config.js
+++ LEVEL0-TASK7/todolist/hardhat.config.js
@@ -6,8 +6,7 @@ module.exports = {
     networks: {
         hardhat: {},
         sepolia: {
-            url:
-                "https://sepolia.infura.io/v3/" + process.env.INFURA_ID, accounts: [`0x${process.env.PRIVATE_KEY}`],
+            url: "https://sepolia.infura.io/v3/" + process.env.INFURA_ID, accounts: [`0x${process.env.PRIVATE_KEY}`],
         },
     },
 };
