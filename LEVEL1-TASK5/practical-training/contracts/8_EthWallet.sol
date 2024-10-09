fbdd64828940559838a25760defc1a863e3cc018
gust <2414566866@qq.com>
2024-10-09 16:25:58 +0800

    LEVEL1-TASK5---实战训练---ETH钱包

---
+++ LEVEL1-TASK5/practical-training/contracts/8_EthWallet.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.17;
+
+contract EthWallet {
+    string storeMsg;
+
+    constructor(){
+    }
+
+    function set(string memory message) public {
+        storeMsg = message;
+    }
+
+    function get() public view returns (string memory) {
+        return storeMsg;
+    }
+}
